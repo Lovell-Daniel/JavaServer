54ac836f7f790f2fb19faa21fa9e63c89e3f0491
Daniel Lovell <daniel.lovell@me.com>
2016-02-26 00:45:25 -0700

    first

---
+++ src/JavaServer.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class JavaServer {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+        //add elements to the ArrayList
+        al.add("JAVA");
+        al.add("C++");
+        al.add("PERL");
+        al.add("PHP");
+        System.out.println(al);
+        //get elements by index
+        System.out.println("Element at index 1: "+al.get(1));
+        System.out.println("Does list contains JAVA? "+al.contains("JAVA"));
+        //add elements at a specific index
+        al.add(2,"PLAY");
+        System.out.println(al);
+        System.out.println("Is arraylist empty? "+al.isEmpty());
+        System.out.println("Index of PERL is "+al.indexOf("PERL"));
+        System.out.println("Size of the arraylist is: "+al.size());
+        
+        ArrayList<String> arrl = new ArrayList<String>();
+        //adding elements to the end
+        arrl.add("First");
+        arrl.add("Second");
+        arrl.add("Third");
+        arrl.add("Random");
+        Iterator<String> itr = arrl.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+	}
+
+}
